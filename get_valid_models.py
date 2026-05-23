d10d63a9a9ad141dae89478f8775648954fd0ea7
U CHARAN TEJ <charanteju2007@gmail.com>
2026-05-23 07:46:15 +0530

    Deleted a file

--- get_valid_models.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-
-api_key="AIzaSyALVQKrwUPgowghuKOZZBOBsEt5hD-gPe0"
-genai.configure(api_key=api_key)
-with open('valid_models.txt', 'w') as f:
-    for m in genai.list_models():
-        if 'generateContent' in m.supported_generation_methods:
-            f.write(m.name + '\n')
